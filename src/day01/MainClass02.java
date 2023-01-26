e192149e27a97321ed748e5a7fea53e64f067255
admin <admin@DESKTOP-3KBVJGI>
2023-01-26 14:13:31 +0900

    첫 커밋

---
+++ src/day01/MainClass02.java
@@ -0,0 +1,15 @@
+package day01;
+//(한줄 주석) 자바 코드 시작 class입니다
+// 자바 코드는 class 예약어 {}안에 정의
+public class MainClass02 {
+/**
+ *  JavaDoc 주석
+ *  
+ *  메인메소드 - 자바 처음 실행되는 메소드
+ * @param args
+ */
+public static void main(String[] args) {
+	// Console 출력 명령어
+	System.out.println("안녕하세요");
+}	
+}
