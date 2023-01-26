7902a6e6556cb96f1a1453d6983a3d6d9e817f2d
KRIT <KRIT@google.com>
2023-01-26 20:03:40 +0900

    day04

---
+++ src/day04/HomeWork2.java
@@ -0,0 +1,27 @@
+package day04;
+
+public class HomeWork2 {
+	public static void main(String[] args) {
+		int[][] array = new int [7][7];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = i*7 + j +1;
+			}			
+		}
+		for(int i= 0; i < 14; i++) {
+			if(i<7) {
+				for(int j = 0; j < i+1; j++) {
+					if(i%2 <1) {
+					System.out.print(array[i-j][j]+"");
+			}else{
+				System.out.print(array[j][i-j]+"");
+		}
+				}
+	}else {
+	}
+	System.out.println();
+		}
+	}
+}
+
+			
